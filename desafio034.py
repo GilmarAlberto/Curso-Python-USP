92d5d01f0f0b04216c74683c0b49931dd130565f
GilmarAlberto <gilabreu00@gmail.com>
2025-12-14 16:27:16 -0300

    for python tópico 1

--- desafio034.py
+++ desafio034.py
@@ -1,30 +1,5 @@
-'''
-Introdução à Ciência da Computação com Python Parte 2
-Módulo 1 - Matrizes
-Tarefa de programação: Lista de exercícios - 1
+#Revisão da Estrutura For!
+#Imprima os números de 1 a 5 usando um for .
 
-https://www.coursera.org/learn/ciencia-computacao-python-conceitos-2/programming/ZP9BZ/lista-de-exercicios-1
-
-Exercício 2: Soma de matrizes
-
-Escreva a função soma_matrizes(m1, m2) que recebe 2 matrizes e devolve uma matriz que represente sua soma caso as matrizes tenham dimensões iguais. Caso contrário, a função deve devolver False.
-'''
-
-def soma_matrizes(m1, m2):
-
-    if len(m1) != len(m2):
-        return false
-
-    soma = []
-
-    for
-
-def main():
-    m1 = [[1, 2, 3], [4, 5, 6]]
-    m2 = [[2, 3, 4], [5, 6, 7]]
-    
-    print(soma_matrizes(m1, m2))
-
-
-if __name__ == "__main__":
-    main()
+for i in [1,2,3,4,5]:
+    print(i)
---
+++ desafio035.py
@@ -0,0 +1,5 @@
+#Revisão da Estrutura For!
+#Percorra uma lista de nomes e exiba cada nome.
+
+for nome in ["Gilmar", "Alberto", "de", "Abreu", "Pinto"]:
+    print(nome)
