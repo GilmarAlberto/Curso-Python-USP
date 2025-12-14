d3e562e86d58b7464ec3addf463e21df3532b6a2
GilmarAlberto <gilabreu00@gmail.com>
2025-12-14 16:52:59 -0300

    for python tópico 2

---
+++ desafio036.py
@@ -0,0 +1,15 @@
+'''
+## 2. `for` com `range()`
+### Explicação
+`range()` gera uma sequência de números. Pode ter 1, 2 ou 3 argumentos.
+
+### Exemplo
+```python
+for i in range(5):
+    print(i)
+
+Exercício 1: Imprima os números de 0 a 10.
+'''
+
+for num in range(11):
+    print(num)
---
+++ desafio037.py
@@ -0,0 +1,18 @@
+"""
+## 2. `for` com `range()`
+### Explicação
+`range()` gera uma sequência de números. Pode ter 1, 2 ou 3 argumentos.
+
+### Exemplo
+```python
+for i in range(5):
+    print(i)
+```
+
+### Exercícios
+
+2. Imprima apenas números pares de 2 a 20.
+"""
+
+for num in range(2,22,2):
+    print(num)
